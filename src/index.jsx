570b6a1989f859d46b98e9f4f8eda93c16c613a9
Nathaniel Knight <nathaniel.ep@gmail.com>
2021-11-10 16:37:50 -0800

    Base of action-rendering element

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,46 @@
 import * as React from 'react';
-import * as Server from 'react-dom/server';
+import { render } from 'react-dom';
 
-let Greet = () => <h1>Hello, world!</h1>;
-console.log(Server.renderToString(<Greet />));
+
+function ChoiceUI(props) {
+    const actions = Array.from(
+        props.storylet.actions.map((action, idx) => {
+            return (<div className="traiter-action" key={idx}>
+                <div
+                    className="action-traiter-body"
+                    dangerouslySetInnerHTML={{ __html: action.body }}
+                >
+                </div>
+                <button onClick={() => props.takeAction(idx)} value="Go" />
+            </div>);
+        })
+    );
+    console.info(actions);
+
+    return (
+        <div className="traiter-choose">
+            <div className="traiter-body"
+                dangerouslySetInnerHTML={{ __html: props.storylet.body }}
+            ></div>
+            <div className="traiter-actions">
+                {actions}
+            </div>
+        </div>
+    );
+}
+
+function init(element) {
+    let storylet = {
+        body: "<p>Once there was a <b>Way</b> to get back <i>Home</i>.</p>",
+        actions: [
+            {body: "<p>Thing the first</p>"},
+            {body: "<p>Thing the second</p>"},
+        ]
+        
+    };
+    render(<ChoiceUI storylet={storylet} takeAction={(idx) => console.info(`took action ${idx}`)}/>, traiter);
+}
+
+
+let e = document.getElementById("traiter");
+init(e);
